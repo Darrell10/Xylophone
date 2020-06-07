9b3cc8868b8e78b2b9cff9985947e41257917fc8
Darrell10 <frederick.port@me.com>
2020-06-07 15:38:35 +0200

    Ajout du code dans viewController, alpha animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
+final class ViewController: UIViewController {
     
-
+    // MARK: - Property
+    
+    var player: AVAudioPlayer!
+    
+    // MARK: - Function
+    
+    /// Play Xylophone sound when push on button
+    private func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()            
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let buttonTittle = sender.currentTitle else {
+            return
+        }
+        playSound(soundName: buttonTittle)
+        // alpha Animation when push button
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
+    
 }
 
